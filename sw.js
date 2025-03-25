073c2daf0413134b6ba636d87fcea02f27c65a84
b2tamilan <111178728+b2tamilan@users.noreply.github.com>
2025-03-25 22:31:38 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+// Cache name
+const CACHE_NAME = 'volumetric-calculator-v1';
+
+// Files to cache
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate the service worker and clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event to serve cached files
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached response if found, otherwise fetch from network
+                return response || fetch(event.request);
+            })
+    );
+});
